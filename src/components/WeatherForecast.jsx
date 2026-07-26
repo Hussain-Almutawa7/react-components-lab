2e7dc30dd7f90714b80ae1a1646b0b298d0dbbb8
Hussain-Almutawa7 <hussain.almutawa99@gmail.com>
2026-07-26 16:24:49 +0300

    Added starter code

---
+++ src/components/WeatherForecast.jsx
@@ -0,0 +1,15 @@
+const WeatherForcast = () => {
+    return (
+        <>
+            <div className="weather">
+                <h2>Day of the Week</h2>
+                <img src="" />
+                <p><span>Conditions: </span>current weather conditions</p>
+                <p><span>Time: </span>time of day</p>
+            </div>
+
+        </>
+    )
+}
+
+export default WeatherForcast;
